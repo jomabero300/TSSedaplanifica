4dc14f7d639cb149a156de120deff9ecef8edcd1
jomabero300 <jomabero300@yahoo.com>
2022-07-21 21:34:59 -0500

    Finish CategoryType CRUD

--- TSSedaplanifica/TSSedaplanifica/Data/Entities/CategoryType.cs
+++ TSSedaplanifica/TSSedaplanifica/Data/Entities/CategoryType.cs
@@ -10,7 +10,7 @@ namespace TSSedaplanifica.Data.Entities
         public int Id { get; set; }
 
         [Column(TypeName = "varchar(100)")]
-        [Display(Name = "Categoria")]
+        [Display(Name = "Clases de categoría")]
         [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; }
